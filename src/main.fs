49da38e279f640ced6cc1e6de3f4d0d689f2c6b1
carrexxii <cjbarrett101@outlook.com>
2024-04-10 12:56:40 +0200

    Basic lexing

--- src/main.fs
+++ src/main.fs
@@ -1,22 +1,16 @@
 ﻿module Main
 
 open System
+open FSharp.Text
 open FSharp.Text.Lexing
 open Lexer
 open Parser
 
-let evaluate input =
-    let lexBuf = LexBuffer<char>.FromString input
-    let output = Parser.parse Lexer.tokenize lexBuf
-    string output
-
 [<EntryPoint>]
 let main argv =
-    while true do
-        printf "Evaluate > "
-        let input = Console.ReadLine ()
-        try
-            printfn $"{evaluate input}"
-        with err -> printfn $"{err}"
+    let testFile = IO.File.ReadAllText "tests/test.kon"
+    let lexbuf = LexBuffer<char>.FromString testFile
+    while not lexbuf.IsPastEndOfStream do
+        printfn $"({tokenize lexbuf}) "
 
     0
